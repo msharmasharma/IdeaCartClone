48366446907c654ab0bcb86b9cf5fb8af026e03e
Madhusudan Sharma <msharma0077865555@gmail.com>
2022-05-05 11:24:33 +0530

    new

--- cloneIdeaCart/my-app/src/components/ProductDescription.jsx
+++ cloneIdeaCart/my-app/src/components/ProductDescription.jsx
@@ -1,18 +1,54 @@
-import styled from "styled-components"
-export const ProductDescription = ()=>{
+import styled from "styled-components";
+export const ProductDescription = () => {
+  const Div = styled.div`
+    display: flex;
+    width: 80%;
+    height: 600px;
+    border: 1px solid black;
+    margin-left: 10%;
 
-    const Div = styled.div`
-        display: flex;
-        width: 80%;
-        height: 600px;
+    #div1 {
+      width: 35%;
+      height: 100%;
+      border: 1px solid red;
+
+      #imgDiv {
+        width: 100%;
+        height: 50%;
         border: 1px solid black;
-        margin-left: 10%;
-    `
-return <div>
-    <h1>Product Description</h1>
-    <Div>
-       <div></div>
-       <div></div>
-    </Div>
-</div>
-}
+      }
+      #belowImg {
+        width: 100%;
+        height: 50%;
+        border: 1px solid teal;
+      }
+    }
+    #div2 {
+      width: 65%;
+      height: 100%;
+      border: 1px solid yellow;
+      #descri {
+        width: 100%;
+        height: 60%;
+        border: 1px solid red;
+      }
+    }
+  `;
+  return (
+    <div>
+      <h1>Product Description</h1>
+      <Div>
+        <div id="div1">
+          <div id="imgDiv">
+            <img src="" alt="Show Here" />
+          </div>
+          <div id="belowImg"></div>
+        </div>
+        <div id="div2">
+          <h1>Book Name Here</h1>
+          <div id="descri">Details Here</div>
+        </div>
+      </Div>
+    </div>
+  );
+};
