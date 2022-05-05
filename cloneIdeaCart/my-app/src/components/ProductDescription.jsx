bf5918c2975cd51029e36de36ecff82defd9ff36
Madhusudan Sharma <msharma0077865555@gmail.com>
2022-05-05 11:12:16 +0530

    new

--- cloneIdeaCart/my-app/src/components/ProductDescription.jsx
+++ cloneIdeaCart/my-app/src/components/ProductDescription.jsx
@@ -1,5 +1,18 @@
+import styled from "styled-components"
 export const ProductDescription = ()=>{
+
+    const Div = styled.div`
+        display: flex;
+        width: 80%;
+        height: 600px;
+        border: 1px solid black;
+        margin-left: 10%;
+    `
 return <div>
     <h1>Product Description</h1>
+    <Div>
+       <div></div>
+       <div></div>
+    </Div>
 </div>
 }
