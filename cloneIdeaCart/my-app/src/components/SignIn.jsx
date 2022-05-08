27e574784d76b6215921ae69aa1b5bf5492fbe0f
Madhusudan Sharma <msharma0077865555@gmail.com>
2022-05-08 12:46:38 +0530

    new

--- cloneIdeaCart/my-app/src/components/SignIn.jsx
+++ cloneIdeaCart/my-app/src/components/SignIn.jsx
@@ -42,23 +42,24 @@ export const SignIn = ()=>{
         `
         
     return (
-        <div className="login">
-            <h2>Log in</h2>
+        <div style={{marginLeft:"10%",textAlign:"left",marginBottom:"10%"}} className="login">
+            <h2 style={{fontSize:"30px"}}>Log in</h2>
             <SignUpDiv className="divone">
                 
                 <div className="labell"> 
-                    <label htmlFor="">Email</label>&ensp;
-                    <input type="text" name="email" value={user.email} onChange={handleChange} ></input>
-                    &ensp; &ensp; &ensp; &ensp;
-              &ensp; &ensp; &ensp;&ensp; &ensp; 
-                    <label htmlFor="">Password</label>&ensp;
-                    <input type="password" name="password" value={user.password} onChange={handleChange} ></input>
+                    <label style={{fontSize:"25px"}} htmlFor="">Email</label>&ensp;<br/>
+                    <input style={{width:"60%",height:"30px"}} type="text" name="email" value={user.email} onChange={handleChange} ></input><br/>
+                    <br/>
+                    <label style={{fontSize:"25px"}} htmlFor="">Password</label>&ensp;<br/>
+                    <input style={{width:"60%",height:"30px"}} type="password" name="password" value={user.password} onChange={handleChange} ></input>
                 </div>
                 <div><span><input type="checkbox"></input></span>Remember me</div>
-                <div className="button signin" onClick={login}>Log in</div>
+                <div style={{border:"1px solid green",width:"60px",backgroundColor:"green",color:"white",textAlign:"center",height:"30px",
+                borderRadius:"3px",paddingTop:"6px"}} 
+                className="button signin" onClick={login}>Log in</div>
                
-                <div className="button registerr" onClick={() => history("/SignIn")}>Sign up</div>
-                <div className="button registerr" >Forgot your Password?</div>
+                <div style={{color:"blue"}} className="button registerr" onClick={() => history("/SignIn")}>Sign up</div>
+                <div style={{color:"blue"}} className="button registerr" >Forgot your Password?</div>
             </SignUpDiv>
 
             
