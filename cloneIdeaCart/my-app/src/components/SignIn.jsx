4a5b955c89fcf8aecac43e9a393614f5b7a5cbc5
jyotiranjan98 <96143764+jyotiranjan98@users.noreply.github.com>
2022-05-08 12:32:16 +0530

    login

--- cloneIdeaCart/my-app/src/components/SignIn.jsx
+++ cloneIdeaCart/my-app/src/components/SignIn.jsx
@@ -1,5 +1,69 @@
+import React, {useState} from "react"
+import styled from "styled-components"
+import axios from "axios"
+import{ useNavigate } from "react-router-dom"
+
 export const SignIn = ()=>{
-    return <div>
-       <h1>SignIn</h1> 
-    </div>
+
+    const history = useNavigate()
+
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleChange = e => {
+        const { name, value } = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
+    const login = () => {
+        axios.post("http://localhost:8080/login", user)
+            .then(res => {
+                alert(res.data.message)
+                // setLoginUser(res.data.user)
+                console.log(res.data.user)
+                console.log(res.data.message)
+                if(res.data.message === "Login Successfull"){
+                    history("/products")
+                }else{
+                    history("/SignIn")
+                }
+                
+
+            })
+    }
+        const SignUpDiv = styled.div`display:grid;
+        grid-template-columns: repeat(1, 1fr);
+        row-gap: 10%;
+        `
+        
+    return (
+        <div className="login">
+            <h2>Log in</h2>
+            <SignUpDiv className="divone">
+                
+                <div className="labell"> 
+                    <label htmlFor="">Email</label>&ensp;
+                    <input type="text" name="email" value={user.email} onChange={handleChange} ></input>
+                    &ensp; &ensp; &ensp; &ensp;
+              &ensp; &ensp; &ensp;&ensp; &ensp; 
+                    <label htmlFor="">Password</label>&ensp;
+                    <input type="password" name="password" value={user.password} onChange={handleChange} ></input>
+                </div>
+                <div><span><input type="checkbox"></input></span>Remember me</div>
+                <div className="button signin" onClick={login}>Log in</div>
+               
+                <div className="button registerr" onClick={() => history("/SignIn")}>Sign up</div>
+                <div className="button registerr" >Forgot your Password?</div>
+            </SignUpDiv>
+
+            
+           
+        </div>
+    )
+    
 }
--- cloneIdeaCart/my-app/src/components/SignUp.jsx
+++ cloneIdeaCart/my-app/src/components/SignUp.jsx
@@ -1,5 +1,72 @@
+import React, { useState } from "react"
+// import "./register.css"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
 export const SignUp = ()=>{
-    return <div>
-       <h1>SignUp</h1>
-    </div>
+   
+
+        const history = useNavigate()
+    
+        const [ user, setUser] = useState({
+            name:"",
+            mobile:"",
+            email:"",
+            password:"",
+            rePassword:"",
+            
+        })
+    
+        const handleChange = e => {
+            const { name, value } = e.target
+            setUser({
+                ...user,
+                [name]: value
+            })
+        }
+    
+        const register = () => {
+            const { name, mobile, email, password, rePassword  } = user
+            if( name && mobile && email && password && (rePassword===password)  ){
+                axios.post("http://localhost:8080/register", user)
+                .then( res => {
+                    alert(res.data.message)
+                    history("/SignIn")
+                })
+            } else {
+                alert("invlid input")
+            }
+            
+        }
+    
+        return (
+            <div className="register">
+                <div className="divone">
+                <h4>Sign up</h4>
+                    {console.log("User", user)}
+                    <div className="label">
+                        <label className="label" >Name</label>
+                        <input type="text" name="name" value={user.name} onChange={handleChange}></input>
+                        <label className="label" >Mobile Number</label>
+                        <input type="text" name="mobile" value={user.mobile} onChange={handleChange}></input>
+                        <label className="label" htmlFor="">Email address</label>
+                        <input type="text" name="email" value={user.email}  onChange={ handleChange }></input>
+                        <label className="label" htmlFor="">Password</label>
+                        <input type="password" name="password" value={user.password}  onChange={ handleChange }></input>
+                        <label className="label" htmlFor="">Password Confirmation</label>
+                        <input type="password" name="rePassword" value={user.rePassword}  onChange={ handleChange }></input>
+                    </div>
+                    <div className="ticbox" style={{display : "flex"}}>
+                    </div>
+                    <div className="button signup" onClick={register} >Sign up</div>
+                    
+                    <div className="button log" onClick={() => history("/")}>Login</div>
+    
+                   
+                </div>
+    
+                
+                
+            </div>
+        )
 }
